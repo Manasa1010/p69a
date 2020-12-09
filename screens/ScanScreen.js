7c1b9ddde673c69a7a449653d9b745eca884c3ae
Manasa <Manasa9955j@gmail.com>
2020-12-09 22:33:38 +0530

    p69b

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -13,7 +13,7 @@ export default class ScanScreen extends React.Component{
             buttonState:'normal'
         }
     }
-    getCameraPermission=async()=>{
+    getCameraPermissions=async()=>{
         const {status}=await Permissions.askAsync(Permissions.CAMERA);
         this.setState({
             hasCameraPermissions:status==="granted",
@@ -25,10 +25,10 @@ handleBarCodeScanner=async({type,data})=>{
         scanned:true,
         scannedData:data,
         buttonState:"normal"
-    })
+    })                                
 }
 render(){
-    const  hasCameraPermission=this.state.hasCameraPermission;
+    const  hasCameraPermissions=this.state.hasCameraPermissions;
     const scanned=this.state.scanned;
     const buttonState=this.state.buttonState;
 
@@ -39,41 +39,17 @@ render(){
             style={StyleSheet.absoluteFillObject}
             ></BarCodeScanner>
         )
-    }else if(buttonState==="normal"){
+    }else if (buttonState==="normal"){
         return(
-            <View style={StyleSheet.container}>
-           <Text style={styles.displayText}>{
-               hasCameraPermissions===true?this.state.scannedData:"Request Camera Permission"
-           }
-           </Text>
-           <TouchableOpacity
-           onPress={this.getCameraPermissions}> 
-           style={styles.scanButton}
-           <Text style={styles.scanButton}>Scan QR Code</Text>
-          
-           </TouchableOpacity>
-
+            <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+             <Text>{hasCameraPermissions===true?this.state.scannedData:"requestCameraPermission"}</Text>
+             <TouchableOpacity onPress={this.getCameraPermissions}><Text>Scan QR Code</Text></TouchableOpacity>   
             </View>
-        )
+            )
+    }
+           
+        
     }
 }
     
-}
-var styles=StyleSheet.create({
-    inputBox:{
-        width:150,
-        height:50,
-        borderWidth:2
-    },
-    scanButton:{
-        width:150,
-        height:50,
-        borderWidth:2
-    },inputView:{
-        flexDirection:"row",
-margin:20
-    },
-    container:{
-        flex:1,justifyContent:"center",alignItems:"center"
-    }
-})
+
